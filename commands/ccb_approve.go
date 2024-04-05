dcd51b375ced82cb95c4a6cff82dc36bbe0d9661
Javier Alvarez Garcia <ja@daedalean.ai>
2024-04-05 12:22:07 +0200

    Additional checks for CCB approval and block
    Only allow approval and block of a ticket that is in a state that can
    transition to the target state being approved/blocked.
    
    This should prevent unintentional issues by which a CCB user would set the
    approval of the accepted state, while they meant to set only the vetted
    state.
    
    Additionally, if possible, attempt to automatically transition to the target state,
    assuming that the CCB validation passes (when there was only 1 CCB
    member left to approve a state).

--- commands/ccb_approve.go
+++ commands/ccb_approve.go
@@ -44,7 +44,8 @@ func runCcbApprove(env *Env, args []string) error {
 
 	// Perform some checks before approving the status of the ticket:
 	//   is the current user an approver of the ticket status?
-	//   has the current user already approved the ticket?
+	//   has the current user already approved the ticket status?
+	//   is the ticket in a status that can transition to the given ticket status?
 
 	currentUserIdentity, err := env.backend.GetUserIdentity()
 	if err != nil {
@@ -61,6 +62,26 @@ func runCcbApprove(env *Env, args []string) error {
 		return nil
 	}
 
+	nextStatusMatchesRequestedApproval := func(currentStatus, nextStatus bug.Status, workflow *bug.Workflow) bool {
+		for _, status := range workflow.NextStatuses(currentStatus) {
+			if status == nextStatus {
+				return true
+			}
+		}
+		return false
+	}
+
+	workflow := bug.FindWorkflow(b.Snapshot().Labels)
+	if workflow == nil {
+		return fmt.Errorf("Could not find associated workflow for ticket %v", b.Id())
+	}
+
+	if !nextStatusMatchesRequestedApproval(b.Snapshot().Status, status, workflow) {
+		// Prevent accidental approval of states, when the ticket is not in a state that transitions to the
+		// requested state
+		return fmt.Errorf("Requested CCB approval for ticket status %s, but ticket is in status %s, which cannot directly transition to %s", status, b.Snapshot().Status, status)
+	}
+
 	// Everything looks ok, approve
 
 	_, err = b.CcbApprove(status)
@@ -69,6 +90,17 @@ func runCcbApprove(env *Env, args []string) error {
 	}
 
 	fmt.Printf("Approving ticket %s\n", b.Id().Human())
+
+	// Attempt to transition to the approved state, but warn if there are more approvals required.
+	if err := bug.ValidateCcb(b.Snapshot(), status); err == nil {
+		_, err = b.SetStatus(status)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Set status to %s\n", status)
+	} else {
+		fmt.Printf("Warning: did not transition the ticket to %s: %v\n", status, err)
+	}
 
 	return b.Commit()
 }
