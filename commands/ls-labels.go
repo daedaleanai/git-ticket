c7f7f8c5bd9962d8303763cc204c07d8061b954d
Ian Whittington <iw@daedalean.ai>
2024-05-31 16:11:12 +0200

    Add pattern argument to ls-label command

--- commands/ls-labels.go
+++ commands/ls-labels.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/spf13/cobra"
 )
@@ -10,29 +11,40 @@ func newLsLabelCommand() *cobra.Command {
 	env := newEnv()
 
 	cmd := &cobra.Command{
-		Use:   "ls-label",
-		Short: "List valid labels.",
-		Long: `List valid labels.
-
-Note: in the future, a proper label policy could be implemented where valid labels are defined in a configuration file. Until that, the default behavior is to return the list of labels already used.`,
+		Use:      "ls-label [pattern]",
+		Short:    "List valid labels.",
+		Long:     `List valid labels, an optional regexp pattern limits the output.`,
 		PreRunE:  loadBackend(env),
 		PostRunE: closeBackend(env),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return runLsLabel(env)
+			return runLsLabel(env, args)
 		},
 	}
 
 	return cmd
 }
 
-func runLsLabel(env *Env) error {
+func runLsLabel(env *Env, args []string) error {
 	labels, err := env.backend.ValidLabels()
 	if err != nil {
 		return fmt.Errorf("Error reading the list of valid labels: %s", err)
 	}
 
-	for _, l := range labels {
-		env.out.Println(l)
+	if len(args) == 0 {
+		// No pattern provided, output full list
+		for _, l := range labels {
+			env.out.Println(l)
+		}
+	} else {
+		pattern, err := regexp.Compile(args[0])
+		if err != nil {
+			return fmt.Errorf("Error compiling pattern: %s", err)
+		}
+		for _, l := range labels {
+			if pattern.MatchString(l.String()) {
+				env.out.Println(l)
+			}
+		}
 	}
 
 	return nil
