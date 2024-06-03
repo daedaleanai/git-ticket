7e1b074b12e217f36dcfcbbcf17da173dbe37785
Javier Alvarez Garcia <ja@daedalean.ai>
2024-06-03 22:00:25 +0200

    display comments as a popup when moving mouse over

--- webui/static/checklist.js
+++ webui/static/checklist.js
@@ -14,3 +14,29 @@ function selectReviewer(tabIdx) {
     const checklistEl = document.getElementById('checklist-' + tabIdx)
     checklistEl.style.display = 'block';
 };
+
+window.onload = function() {
+    for (const el of document.getElementsByClassName('checklist-state')) {
+        el.addEventListener('mouseenter', (e) => {
+            const usr = el.getAttribute('data-usr-id');
+            const section = el.getAttribute('data-section-id');
+            const question = el.getAttribute('data-question-id');
+            const id =
+                'comment-' + usr + '-sec-' + section + '-question-' + question;
+
+            const commentEl = document.getElementById(
+                'comment-' + usr + '-sec-' + section + '-question-' + question);
+
+            if (commentEl.innerText !== '') {
+                const alertEl = document.getElementById('alert');
+                alertEl.style.display = 'block';
+                alertEl.innerText = commentEl.innerText;
+            }
+        });
+
+        el.addEventListener('mouseleave', (e) => {
+            const alertEl = document.getElementById('alert');
+            alertEl.style.display = 'none';
+        });
+    }
+};
--- webui/static/index.js
+++ webui/static/index.js
@@ -1,28 +1,31 @@
-"use strict";
+'use strict';
 
-window.onload = function () {
-    const alertEl = document.getElementById("alert");
-    alertEl.addEventListener("click", (e) => {
-        alertEl.style.display = "none";
+window.onload = function() {
+    const alertEl = document.getElementById('alert');
+    alertEl.addEventListener('click', (e) => {
+        alertEl.style.display = 'none';
     });
 
-    for (const ticketEl of document.getElementsByClassName("gt-ticket")) {
+    for (const ticketEl of document.getElementsByClassName('gt-ticket')) {
         ticketEl.draggable = true;
-        ticketEl.addEventListener("dragstart", (e) => {
-            e.dataTransfer.setData("text/plain", e.target.id);
+        ticketEl.addEventListener('dragstart', (e) => {
+            e.dataTransfer.setData('text/plain', e.target.id);
 
-            const placeholderEl = document.getElementById("drag-drop-placeholder");
-            placeholderEl.style.display = "block";
-            placeholderEl.style.height = e.target.getBoundingClientRect().height + 'px';
+            const placeholderEl =
+                document.getElementById('drag-drop-placeholder');
+            placeholderEl.style.display = 'block';
+            placeholderEl.style.height =
+                e.target.getBoundingClientRect().height + 'px';
         });
     }
 
-    for (const columnEl of document.getElementsByClassName("gt-column-body")) {
-        columnEl.addEventListener("dragover", (e) => {
+    for (const columnEl of document.getElementsByClassName('gt-column-body')) {
+        columnEl.addEventListener('dragover', (e) => {
             e.preventDefault();
-            e.dataTransfer.dropEffect = "move";
+            e.dataTransfer.dropEffect = 'move';
 
-            const placeholderEl = document.getElementById("drag-drop-placeholder");
+            const placeholderEl =
+                document.getElementById('drag-drop-placeholder');
             for (const childEl of columnEl.children) {
                 const rect = childEl.getBoundingClientRect()
                 if ((rect.top + rect.bottom) / 2 > e.clientY) {
@@ -33,36 +36,37 @@ window.onload = function () {
             columnEl.insertAdjacentElement('beforeend', placeholderEl);
         });
 
-        columnEl.addEventListener("drop", async (e) => {
+        columnEl.addEventListener('drop', async (e) => {
             e.preventDefault();
 
-            const ticket = e.dataTransfer.getData("text/plain");
+            const ticket = e.dataTransfer.getData('text/plain');
             const status = columnEl.dataset.status;
 
-            const resp = await fetch("/api/set-status", {
-                method: "POST",
+            const resp = await fetch('/api/set-status', {
+                method: 'POST',
                 body: JSON.stringify({
                     ticket,
                     status,
                 })
             });
             const text = await resp.text();
-            
-            const placeholderEl = document.getElementById("drag-drop-placeholder");
-            placeholderEl.style.display = "none";
 
-            const alertEl = document.getElementById("alert");
-            alertEl.style.display = "block";
-            alertEl.classList.remove("alert-danger");
-            alertEl.classList.remove("alert-success");
+            const placeholderEl =
+                document.getElementById('drag-drop-placeholder');
+            placeholderEl.style.display = 'none';
+
+            const alertEl = document.getElementById('alert');
+            alertEl.style.display = 'block';
+            alertEl.classList.remove('alert-danger');
+            alertEl.classList.remove('alert-success');
             alertEl.innerText = text;
 
             if (resp.ok) {
                 const ticketEl = document.getElementById(ticket);
                 placeholderEl.insertAdjacentElement('beforebegin', ticketEl);
-                alertEl.classList.add("alert-success");
+                alertEl.classList.add('alert-success');
             } else {
-                alertEl.classList.add("alert-danger");
+                alertEl.classList.add('alert-danger');
             }
         });
     }
