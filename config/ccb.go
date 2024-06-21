715ca129451c8ba15f8b8d61508f7b75072a64d4
Javier Alvarez Garcia <ja@daedalean.ai>
2024-06-21 19:31:16 +0200

    Introduce CCB teams
    A new configuration item `ccb-teams` that will supersede the old `ccb`
    configuration. It includes CCB teams and names for each CCB identity.

--- config/ccb.go
+++ config/ccb.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 
 	"github.com/daedaleanai/git-ticket/entity"
@@ -10,11 +9,21 @@ import (
 	"github.com/daedaleanai/git-ticket/repository"
 )
 
-type CcbConfig []entity.Id
+type CcbMember struct {
+	Name entity.Id
+	Id   entity.Id
+}
 
-// LoadCcbConfig attempts to read the ccb group out of the current repository and store it in ccbMembers
+type CcbTeam struct {
+	Name    string
+	Members []CcbMember
+}
+
+type CcbConfig []CcbTeam
+
+// readCcbMembers attempts to read the ccb group out of the current repository and store it in ccbTeams
 func LoadCcbConfig(repo repository.ClockedRepo) (CcbConfig, error) {
-	ccbData, err := GetConfig(repo, "ccb")
+	ccbData, err := GetConfig(repo, "ccb-teams")
 	if err != nil {
 		if _, ok := err.(*NotFoundError); ok {
 			return CcbConfig{}, nil
@@ -22,30 +31,50 @@ func LoadCcbConfig(repo repository.ClockedRepo) (CcbConfig, error) {
 		return nil, fmt.Errorf("unable to read ccb config: %q", err)
 	}
 
-	// Parse the CCB member list from the configuration. Configurations must be of the form "map[string]interface{}" so
-	// is stored as {"ccbMembers" : ["<user id1>", "<user id2>", "..."]}.
-	ccbMembersTemp := make(map[string][]entity.Id)
+	type ccbTeamsJson map[string][]CcbMember
 
-	err = json.Unmarshal(ccbData, &ccbMembersTemp)
+	type config struct {
+		Teams ccbTeamsJson `json:"ccbTeams"`
+	}
+
+	ccbTeamsTemp := config{}
+
+	err = json.Unmarshal(ccbData, &ccbTeamsTemp)
 	if err != nil {
 		return nil, fmt.Errorf("unable to load ccb: %q", err)
 	}
 
-	var present bool
-	ccbMembers, present := ccbMembersTemp["ccbMembers"]
-	if !present {
-		return nil, errors.New("unexpected ccb config format")
+	ccbTeams := []CcbTeam{}
+	for name, members := range ccbTeamsTemp.Teams {
+		ccbTeams = append(ccbTeams, CcbTeam{
+			Name:    name,
+			Members: members,
+		})
 	}
 
-	return ccbMembers, nil
+	return ccbTeams, nil
 }
 
 // IsCcbMember returns a flag indicating if the user is a ccb member, as defined in the repository configuration
 func (c CcbConfig) IsCcbMember(user identity.Interface) (bool, error) {
-	for _, c := range c {
-		if c == user.Id() {
-			return true, nil
+	for _, team := range c {
+		for _, member := range team.Members {
+			if member.Id == user.Id() {
+				return true, nil
+			}
 		}
 	}
 	return false, nil
+}
+
+// ListCcbMembers returns a list of CCB members
+func (c CcbConfig) ListCcbMembers() ([]entity.Id, error) {
+	members := []entity.Id{}
+	for _, team := range c {
+		for _, member := range team.Members {
+			members = append(members, member.Id)
+		}
+	}
+
+	return members, nil
 }
