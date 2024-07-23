a4639a47e85993e44bc128b5b81a18b634486568
Javier Alvarez Garcia <ja@daedalean.ai>
2024-07-23 22:53:12 +0200

    Ensure that reviewers of a ticket actually get added as actors
    Note that a cache rebuild is needed for this to work

--- bug/op_set_review_test.go
+++ bug/op_set_review_test.go
@@ -2,65 +2,77 @@ package bug
 
 import (
 	"encoding/json"
-	review2 "github.com/daedaleanai/git-ticket/bug/review"
 	"testing"
 	"time"
+
+	"github.com/daedaleanai/git-ticket/bug/review"
 
 	"github.com/daedaleanai/git-ticket/identity"
 	"github.com/stretchr/testify/assert"
 )
 
-var testUpdates = []review2.ReviewUpdate{
-	review2.ReviewUpdate{
-		PhabTransaction: review2.PhabTransaction{
+var rene = identity.NewBare("René Descarte", "rene@descartes.fr")
+
+var testUpdates = []review.ReviewUpdate{
+	{
+		PhabTransaction: review.PhabTransaction{
 			TransId:   "10000",
 			PhabUser:  "USERID1",
 			Timestamp: 0,
-			Type:      review2.StatusTransaction,
-			Status:    "in progress"}},
-	review2.ReviewUpdate{
-		PhabTransaction: review2.PhabTransaction{
+			Type:      review.StatusTransaction,
+			Status:    "in progress"},
+		AuthorId: rene,
+	},
+	{
+		PhabTransaction: review.PhabTransaction{
 			TransId:   "10005",
 			PhabUser:  "USERID1",
 			Timestamp: 5,
-			Type:      review2.StatusTransaction,
-			Status:    "on review"}},
-	review2.ReviewUpdate{
-		PhabTransaction: review2.PhabTransaction{
+			Type:      review.StatusTransaction,
+			Status:    "on review"},
+		AuthorId: rene,
+	},
+	{
+		PhabTransaction: review.PhabTransaction{
 			TransId:   "10010",
 			PhabUser:  "USERID1",
 			Timestamp: 10,
-			Type:      review2.StatusTransaction,
-			Status:    "complete"}},
-	review2.ReviewUpdate{
-		PhabTransaction: review2.PhabTransaction{
+			Type:      review.StatusTransaction,
+			Status:    "complete"},
+		AuthorId: rene,
+	},
+	{
+		PhabTransaction: review.PhabTransaction{
 			TransId:   "10001",
 			PhabUser:  "USERID2",
 			Timestamp: 1,
-			Type:      review2.CommentTransaction,
+			Type:      review.CommentTransaction,
 			Diff:      123,
 			Path:      "code/under_test.go",
 			Line:      1,
-			Text:      "needs work"}},
-	review2.ReviewUpdate{
-		PhabTransaction: review2.PhabTransaction{
+			Text:      "needs work"},
+		AuthorId: rene,
+	},
+	{
+		PhabTransaction: review.PhabTransaction{
 			TransId:   "10002",
 			PhabUser:  "USERID2",
 			Timestamp: 2,
-			Type:      review2.CommentTransaction,
+			Type:      review.CommentTransaction,
 			Diff:      124,
 			Path:      "code/under_test.go",
 			Line:      1,
-			Text:      "LGTM"}},
+			Text:      "LGTM"},
+		AuthorId: rene,
+	},
 }
 
 func TestOpSetReview_SetReview(t *testing.T) {
-	var rene = identity.NewBare("René Descarte", "rene@descartes.fr")
 	unix := time.Now().Unix()
 	bug1 := NewBug()
 
 	before, err := SetReview(bug1, rene, unix,
-		&review2.PhabReviewInfo{RevisionId: "D1234",
+		&review.PhabReviewInfo{RevisionId: "D1234",
 			LastTransaction: "12345",
 			Updates:         testUpdates,
 		})
@@ -87,23 +99,23 @@ func TestOpSetReview_Apply(t *testing.T) {
 	snapshot := NewBug().Compile()
 
 	// create an operation and apply to the snapshot
-	setReviewOp := NewSetReviewOp(rene, unix, &review2.PhabReviewInfo{RevisionId: "D1234",
+	setReviewOp := NewSetReviewOp(rene, unix, &review.PhabReviewInfo{RevisionId: "D1234",
 		LastTransaction: "12345",
-		Updates:         []review2.ReviewUpdate{testUpdates[0]}})
+		Updates:         []review.ReviewUpdate{testUpdates[0]}})
 	setReviewOp.Apply(&snapshot)
 
 	// sumation holds a local copy of what should be in the snapshot
-	sumation := &review2.PhabReviewInfo{RevisionId: "D1234",
+	sumation := &review.PhabReviewInfo{RevisionId: "D1234",
 		LastTransaction: "12345",
-		Updates:         []review2.ReviewUpdate{testUpdates[0]},
+		Updates:         []review.ReviewUpdate{testUpdates[0]},
 	}
 
 	assert.Equal(t, sumation, snapshot.Reviews["D1234"])
 
 	// add an extra Update
-	setReviewOp2 := NewSetReviewOp(rene, unix, &review2.PhabReviewInfo{RevisionId: "D1234",
+	setReviewOp2 := NewSetReviewOp(rene, unix, &review.PhabReviewInfo{RevisionId: "D1234",
 		LastTransaction: "12346",
-		Updates:         []review2.ReviewUpdate{testUpdates[1]}})
+		Updates:         []review.ReviewUpdate{testUpdates[1]}})
 	setReviewOp2.Apply(&snapshot)
 
 	sumation.Updates = append(sumation.Updates, testUpdates[1])
@@ -112,7 +124,7 @@ func TestOpSetReview_Apply(t *testing.T) {
 	assert.Equal(t, sumation, snapshot.Reviews["D1234"])
 
 	// and a couple more
-	setReviewOp3 := NewSetReviewOp(rene, unix, &review2.PhabReviewInfo{RevisionId: "D1234",
+	setReviewOp3 := NewSetReviewOp(rene, unix, &review.PhabReviewInfo{RevisionId: "D1234",
 		LastTransaction: "12347",
 		Updates:         testUpdates[1:2]})
 	setReviewOp3.Apply(&snapshot)
@@ -123,7 +135,7 @@ func TestOpSetReview_Apply(t *testing.T) {
 	assert.Equal(t, sumation, snapshot.Reviews["D1234"])
 
 	// remove the review
-	setReviewOp4 := NewSetReviewOp(rene, unix, &review2.RemoveReview{ReviewId: "D1234"})
+	setReviewOp4 := NewSetReviewOp(rene, unix, &review.RemoveReview{ReviewId: "D1234"})
 	setReviewOp4.Apply(&snapshot)
 
 	assert.NotContains(t, snapshot.Reviews, "D1234")
